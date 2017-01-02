bfb673d8d22fdd19dcfc70da5ef025d3de506686
luwanchuan <luwanchuan@bestpay.com.cn>
2017-01-02 17:02:08 +0800

    商品信息查询

---
+++ src/main/java/team/yqby/platform/common/constant/FlowConstant.java
@@ -0,0 +1,13 @@
+package team.yqby.platform.common.constant;
+
+/**
+ * 常量
+ * Author: luwanchuan
+ * Date: 2017/1/2
+ */
+public class FlowConstant {
+
+    /** 操作员：系统默认SYSTEM */
+    public static final String OPERATOR_SYSTEM = "SYSTEM";
+
+}
