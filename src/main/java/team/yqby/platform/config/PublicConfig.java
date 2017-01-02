72e5bbc7a8c7b4bae1c3ed69299ffc584231f663
jumping <yinjiangping@bestpay.com.cn>
2017-01-02 11:56:56 +0800

    修改文件路径
    Signed-off-by: jumping <yinjiangping@bestpay.com.cn>

--- src/main/java/team/yqby/platform/config/PublicConfig.java
+++ src/main/java/team/yqby/platform/config/PublicConfig.java
@@ -3,8 +3,8 @@ package team.yqby.platform.config;
 
 public class PublicConfig {
     //账单文件路径
-//    public static final String BILL_FILE_PATH = "/data/file/";
-    public static final String BILL_FILE_PATH = "D:\\";
+    public static final String BILL_FILE_PATH = "/data/file/";
+//    public static final String BILL_FILE_PATH = "D:\\";
     //账单地址
     public static final String  BILL_HOME_URL = "http://service.sh.189.cn";
     //图片路径
