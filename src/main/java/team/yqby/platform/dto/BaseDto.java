d7deda4475ab2b33185d8759669d8b642c12d0bc
jumping <626828733@qq.com>
2022-06-15 19:01:09 +0800

    last commit

--- src/main/java/team/yqby/platform/dto/BaseDto.java
+++
@@ -1,9 +0,0 @@
-package team.yqby.platform.dto;
-
-/**
- * <p>
- * </p>
- * User：jumping Date： 2016/10/20 0020 ProjectName:settlement Version：1.0
- */
-public class BaseDto {
-}
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-<head>
-    <title>德佳通信平台</title>
-</head>
-<body>
-  error
-</body>
-</html>
